cd58a27d374a42701386cd1190775cda9e67addc
Jeff Stringer <jeff.j.stringer@gmail.com>
2014-02-07 09:27:04 -0800

    updating analyze.rb

--- txt2csv/lib/analyze.rb
+++ txt2csv/lib/analyze.rb
@@ -1,10 +1,11 @@
-# put your solution to code challenge 02 here
-
+# Read the command line argument and set up for either hunting prefixes or suffixes, like this:
 case ARGV[0]
   when '-p'
     # set up some regular expression for prefixes
-    ARGV
-    	
+
+
+
+
   when '-s'
     # set up some regular expression for suffixes
   else
@@ -14,6 +15,8 @@ case ARGV[0]
 end
 
 # throw away the command line option in ARGV so we can get to STDIN
+
+
 
 # go through STDIN line by line
 #   use the regular expression to find the right word
