f16f658fd0558b880c68c7f21470edc00a0a3f46
Jeff Stringer <jeff.j.stringer@gmail.com>
2014-02-07 09:33:24 -0800

    initial commit of analyze.rb

--- txt2csv/lib/analyze.rb
+++ txt2csv/lib/analyze.rb
@@ -3,7 +3,27 @@ case ARGV[0]
   when '-p'
     # set up some regular expression for prefixes
 
+	def words_from_string(string)
+		string.downcase.scan(/^[\w'\.]+/)
+	end  
 
+	def count_frequency(words_from_string)
+	  counts = Hash.new(0)
+	  for word in words_from_string
+	    counts[word] +=1
+	  end
+	  counts
+	end
+
+	p count_frequency(words_from_string("
+	Mr. Jones
+	Miss Smith
+	Mrs. Wesson
+	Dr. Roberts
+	Miss Smith
+	Jane Wintermute
+	Mrs. Wesson
+	Frank Franklin"))
 
 
   when '-s'
