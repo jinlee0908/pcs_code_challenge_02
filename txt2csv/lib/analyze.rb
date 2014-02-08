ea9a0e8369bed6a6e201a39589e7d934e9c7a575
JinLee <jinlee0908@gmail.com>
2014-02-07 21:34:34 -0800

    First pass at challenge

--- txt2csv/lib/analyze.rb
+++ txt2csv/lib/analyze.rb
@@ -1,11 +1,13 @@
-# put your solution to code challenge 02 here
+## put your solution to code challenge 02 here
+require 'pry'
 
 case ARGV[0]
+
   when '-p'
+    pattern = /^\w+/ 
     # set up some regular expression for prefixes
-    ARGV
-    	
   when '-s'
+    pattern = /\w+$/
     # set up some regular expression for suffixes
   else
     puts "unknown option"
@@ -13,12 +15,27 @@ case ARGV[0]
     exit
 end
 
-# throw away the command line option in ARGV so we can get to STDIN
+# ???? throw away the command line option in ARGV so we can get to STDIN ???
 
 # go through STDIN line by line
-#   use the regular expression to find the right word
-#   count the word using a hash
 
+counts = Hash.new(0)
+STDIN.each_line do |line| 
+  match = line[pattern]
+  counts[match] += 1
+end
+
+sorted = counts.sort_by {|word,count| count}.reverse
+
+puts sorted.map {|word, count| "#{word} : #{count}"}
+
+
+#   use the regular expression to find the right word#   count the word using a hash
 # after you've read all the lines, sort the hash so the most frequent words are first
-
 # write the hash to STDIN
+
+
+
+
+
+
