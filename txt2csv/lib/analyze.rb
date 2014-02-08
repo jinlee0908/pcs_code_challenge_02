1ea099d60c4865c4f7031d55fb0e98eb0fc67f91
Jeff Stringer <jeff.j.stringer@gmail.com>
2014-02-08 09:08:20 -0800

    update of prefix and suffix extraction and to sorted hash

--- txt2csv/lib/analyze.rb
+++ txt2csv/lib/analyze.rb
@@ -1,10 +1,13 @@
 # Read the command line argument and set up for either hunting prefixes or suffixes, like this:
 case ARGV[0]
+ 
+# Outputs prefixes in format which matches what test requires, but still outputs hash: refactor!  Next to tackle IO.
   when '-p'
-    # set up some regular expression for prefixes
+   
+ 	require 'pry'
 
-	def words_from_string(string)
-		string.downcase.scan(/^[\w'\.]+/)
+def words_from_string(string)
+		string.scan(/^[\w'\.]+/)
 	end  
 
 	def count_frequency(words_from_string)
@@ -12,22 +15,96 @@ case ARGV[0]
 	  for word in words_from_string
 	    counts[word] +=1
 	  end
-	  counts
+	  counts_sorted = counts.sort { |a,b| b.last <=> a.last }
+	  counts_sorted.each {|key, value| puts "#{key} #{value}\n" }
 	end
 
 	p count_frequency(words_from_string("
-	Mr. Jones
-	Miss Smith
-	Mrs. Wesson
-	Dr. Roberts
-	Miss Smith
-	Jane Wintermute
-	Mrs. Wesson
-	Frank Franklin"))
+Mr. Jones
+Mr. Jones
+Mr. Jones
+Mr. Jones
+Mr. Jones
+Miss Smith
+Miss Smith
+Miss Smith
+Miss Smith
+Miss Smith
+Miss Smith
+Mrs. Wesson
+Mrs. Wesson
+Mrs. Wesson
+Mrs. Wesson
+Dr. Roberts
+Dr. Roberts
+Dr. Roberts
+Dr. Roberts
+Dr. Roberts
+Dr. Roberts
+Dr. Roberts
+Dr. Roberts
+Dr. Roberts
+Dr. Roberts
+Jane Wintermute
+Frank Franklin
+Frank Franklin
+Darleen Washington
+Darleen Washington
+Darleen Washington"))
 
-
+ 
+# Outputs suffixes in format which matches what test requires, but still outputs hash: refactor!  Next to tackle IO.
   when '-s'
-    # set up some regular expression for suffixes
+
+require 'pry'
+
+    def words_from_string(string)
+		string.scan(/[\w']+$/)
+	end  
+
+	def count_frequency(words_from_string)
+	  counts = Hash.new(0)
+	  for word in words_from_string
+	    counts[word] +=1
+	  end
+	  counts_sorted = counts.sort { |a,b| b.last <=> a.last }
+	  counts_sorted.each {|key, value| puts "#{key} #{value}\n" }
+	end
+
+	p count_frequency(words_from_string("
+Mr. Jones
+Mr. Jones
+Mr. Jones
+Mr. Jones
+Mr. Jones
+Miss Smith
+Miss Smith
+Miss Smith
+Miss Smith
+Miss Smith
+Miss Smith
+Mrs. Wesson
+Mrs. Wesson
+Mrs. Wesson
+Mrs. Wesson
+Dr. Roberts
+Dr. Roberts
+Dr. Roberts
+Dr. Roberts
+Dr. Roberts
+Dr. Roberts
+Dr. Roberts
+Dr. Roberts
+Dr. Roberts
+Dr. Roberts
+Jane Wintermute
+Frank Franklin
+Frank Franklin
+Darleen Washington
+Darleen Washington
+Darleen Washington"))
+
+    
   else
     puts "unknown option"
     puts "usage: analyze.rb -p | -s < input_file > output_file"
